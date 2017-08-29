8a7b60f02ce2ff694e4a4e8d63b082223e7a73fb
Venditti <Michael.Venditti@ontario.ca>
2017-08-29 17:03:50 -0400

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,86 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            //init dummy data
+
+            Cities = new List<CityDto>()
+            {
+                new CityDto
+                {
+                    Id = 1,
+                    Name = "NYC",
+                    Description = "The city with the big park",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Big ass park"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "Big ass building"
+                        }
+                    }
+                },
+                new CityDto
+                {
+                    Id = 2,
+                    Name = "StC",
+                    Description = "St. Catharines baby",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Montebello Park",
+                            Description = "Lame ass park"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "MTO Building",
+                            Description = "Work ass building"
+                        }
+                    }
+                },
+                new CityDto
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "So many nice things to see and eat",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Louvre",
+                            Description = "Big ass museum"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Eiffel",
+                            Description = "Big pointy tower"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace CityInfo.API
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()
+                    ));
+
+                //.AddJsonOptions(o => {
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        castResolver.NamingStrategy = null;
+                //    }
+                //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseMvc();
+
+            //app.Run((context) =>
+            //{
+            //    throw new Exception("Example exception");
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}

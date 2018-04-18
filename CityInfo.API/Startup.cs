7e8567d3f65eb5a328ed6cb2044323089efe3522
Mike Venditti <matteblackfinish@gmail.com>
2018-04-18 14:24:13 -0400

    Module 7 part 1... adding Repository using Interface

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -59,6 +59,8 @@ namespace CityInfo.API
             //var connectionString = @"Server=(localdb)\ProjectsV13;Database=CityInfoDB;Trusted_Connection=True;";
             var connectionString = Startup.Configuration["connectionStrings:cityInfoDBConnectionString"];
             services.AddDbContext<CityInfoContext>(o=>o.UseSqlServer(connectionString));
+
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
